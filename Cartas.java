a73f20f06044eb06d3f04f8da5aa982ad1031547
pedroarduini <116195191+pedroarduini@users.noreply.github.com>
2022-10-19 13:55:53 -0300

    Update Cartas.java

--- Cartas.java
+++ Cartas.java
@@ -21,4 +21,22 @@ class Cartas {
 	public String getNome() {
 		return this.nome;
 	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (tipo != other.tipo)
+			return false;
+		return true;
+	}
 }
