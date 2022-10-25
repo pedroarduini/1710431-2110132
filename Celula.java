d0522b4c40bceaf30cb2d32195da500880bb08ed
pedroarduini <116195191+pedroarduini@users.noreply.github.com>
2022-10-25 17:51:01 -0300

    Update Celula.java

--- Celula.java
+++ Celula.java
@@ -4,16 +4,24 @@ import java.io.FileReader;
 import java.io.IOException;
 
 protected class Celula{
-  char identificador;
-  int linha;
-  int coluna;
-  boolean temJogador = false;
+  private char identificador;
+  private int linha;
+  private int coluna;
+  private Comodo comodo;
+  private boolean temJogador = false;
   
   public Celula(char i, int l, int c){
     this.identificador = i;
     this.linha = l;
     this.coluna = c;
-  }  
+    if (i == 'R'){
+      comodo = achaComodo(l,c);  
+    }  
+  }
+  
+  public Celula(){
+    this.identificador = 'S';  
+  }   
   
   public getLinha(){
     return linha; 
