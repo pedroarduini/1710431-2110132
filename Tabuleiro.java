68df6c4f437b133c34c0beb24437a1ab4e3d1f5f
pedroarduini <116195191+pedroarduini@users.noreply.github.com>
2022-10-25 17:29:30 -0300

    Update Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,5 +1,27 @@
 package model;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Tabuleiro {
-
+  ArrayList<ArrayList<Celula>> celulas;
+  
+  public Tabuleiro(){
+    loadCelulas();
+  }  
+  
+  private loadCelulas(){
+    BufferedReader temp = new BufferedReader(new FileReader("celulastabuleiro.txt"));
+    char value;
+    for(int i = 0;i < 24; i++){
+      for(int k = 0; k < 24; k++){
+        value = temp.read();
+        celulas.get(i).add(new Celula(value,i,k));
+      }
+    }  
+    
+  }  
+  
 }
